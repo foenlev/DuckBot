a379b4891c8dec115f8b59b0337603eda5db8330
Viktor Fønlev <66299649+foenlev@users.noreply.github.com>
2023-05-10 17:47:50 +0200

    Update and rename level.js to info.js
    basic info command

---
+++ src/commands/info.js
@@ -0,0 +1,44 @@
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  cooldown: 10,
+	data: new SlashCommandBuilder()
+		.setName('info')
+		.setDescription('get info about you or another duck')
+    .addUserOption(option =>
+  		option
+        .setName('user')
+  			.setDescription('who do you want spy on?')
+    ),
+	async execute(interaction) {
+    const target = interaction.options.getUser('user') || null;
+
+    const replyEmbed = new EmbedBuilder()
+  	 .setTimestamp();
+
+    await interaction.deferReply({ephemeral: true});
+
+    if (target != null) {
+          replyEmbed
+            .setColor('Green')
+            .setDescription('')
+            .setTitle('your Info');
+          interaction.editReply({ content: "", embeds: [replyEmbed] });
+    } else {
+      let counter = 0;
+      let interval = setInterval(() => {
+        interaction.editReply('Gathering Info: '+ progressBar(counter, 100, 30) ); // todo: make this look better lol
+        if (counter >= Math.round(Math.random() * (86 - 30) + 30)) {
+          clearInterval(interval);
+          replyEmbed
+            .setColor('Green')
+            .setDescription('')
+            .setTitle(target.tag +'\'s Info');
+          interaction.editReply({ content: "", embeds: [replyEmbed] });
+        } else {
+          counter += 5;
+        }
+      }, 250)
+    }
+  },
+};
